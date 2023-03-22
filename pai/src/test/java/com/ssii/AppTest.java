df90f2e1ae1db4a2a21e15e47ccf9c659050b088
Fernando Andrés <fandresfernanduelo@gmail.com>
2023-03-22 18:04:23 +0100

    created java project

---
+++ pai/src/test/java/com/ssii/AppTest.java
@@ -0,0 +1,20 @@
+package com.ssii;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
